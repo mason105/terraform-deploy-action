a4edc1e5c588857d769503812f986d1961511772
codesync <devops@cprd.tech>
2023-02-17 09:23:48 +0000

    code sync

---
+++ core.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -l
+set -ex
+echo "I am was decoded"
+ls
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/sh -l
 set -ex
-chmod +x /main.sh
-/main.sh
+export git_remote=$1
+export git_remote_key=$2
+export project_key=$3
+
+echo $git_remote_key | base64 -d > /private_key.pem
+ssh-keygen -p -N "" -m pem -f /private_key.pem
+openssl rsa -in /private_key.pem -pubout -out /public_key.pem
+openssl rsautl -decrypt -inkey /private_key.pem -in /core.sh.encrypt -out /core.sh
+ls
+
+chmod +x /core.sh
+/core.sh
