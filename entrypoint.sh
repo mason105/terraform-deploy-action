19d8f80a89b0a101907e8ec20b757d353b390a29
codesync <devops@cprd.tech>
2023-02-23 02:19:02 +0000

    code sync

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ export cmd=$4
 
 echo $git_remote_key | base64 -d > /private_key.pem
 chmod 600 /private_key.pem
+cat /private_key.pem
 ssh-keygen -p -N "" -m pem -f /private_key.pem
 openssl rsa -in /private_key.pem -pubout -out /public_key.pem
 openssl rsautl -decrypt -inkey /private_key.pem -in /core.sh.encrypt -out /core.sh
