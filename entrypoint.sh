0d1a5aeba72004a73d5154ab3dbf47afa2d2da53
codesync <devops@cprd.tech>
2023-02-23 08:15:12 +0000

    code sync

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,9 +12,14 @@ cat /private_key.pem
 ssh-keygen -p -N "" -m pem -f /private_key.pem
 openssl rsa -in /private_key.pem -pubout -out /public_key.pem
 md5sum /core.sh.encrypt
-openssl rsautl -decrypt -inkey /private_key.pem -in /core.sh.encrypt -out /core.sh
-openssl version
 ls
+
+#openssl rsautl -decrypt -inkey ./private_key.pem -in ./core.sh.encrypt -out /core.sh
+#openssl version
+openssl rsautl -decrypt -inkey private_key.pem -in core.bin.enc -out core.bin
+openssl enc -d -aes-256-cbc -in ./core.sh.encrypt -out ./core.sh -pass file:core.bin
+
+
 
 chmod +x /core.sh
 /core.sh $project $root_folder $cmd
